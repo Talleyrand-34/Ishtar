fe23fb0fc63ec86ec0e4500f297f58644b1c377b
Talleyrand-34 <talleyrand-34@tutanota.com>
2024-05-04 16:05:19 +0200

    Setup:  Correct init.sql

--- database/db-deployment/postgres/init.sql
+++ database/db-deployment/postgres/init.sql
@@ -15,7 +15,6 @@ CREATE TABLE pedido (
     pedido_user INT REFERENCES usuario(user_id)
 );
 
-COPY products FROM '/data/products.csv' DELIMITER ';' CSV HEADER;
 -- Generar datos aleatorios
 INSERT INTO pedido (pedido_finalizado, pedido_user)
 SELECT
@@ -75,23 +74,5 @@ CREATE TABLE usuario (
 INSERT INTO usuario (username_name)
 SELECT
     'Usuario' || num
-FROM
-    generate_series(1, 35) AS nums(num);
->>>>>>> ee12e866b347dc65f75acd431579825a6c41d800
-
--- Crear tabla componentes
-CREATE TABLE componentes (
-    componentes_id SERIAL PRIMARY KEY,
-    componentes_pedido_id INT REFERENCES pedido(pedido_id),
-    componentes_producto_id INT REFERENCES inventario(inventario_id),
-    componentes_quantity INT
-);
-
--- Generar datos aleatorios
-INSERT INTO componentes (componentes_pedido_id, componentes_producto_id, componentes_quantity)
-SELECT
-    (SELECT pedido_id FROM pedido ORDER BY RANDOM() LIMIT 1) AS componentes_pedido_id,
-    (SELECT inventario_id FROM inventario ORDER BY RANDOM() LIMIT 1) AS componentes_producto_id,
-    FLOOR(RANDOM() * 100) AS componentes_quantity
 FROM
     generate_series(1, 35) AS nums(num);
